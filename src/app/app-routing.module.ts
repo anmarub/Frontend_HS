a6d71500ba4e7b6317d111db94b6d191177bcf5e
Andres Rubiano <anmaru@outlook.com>
2021-11-08 19:09:23 -0500

    inicio proyecto angula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //defino mis rutas
+  {
+    path:"inicio", // nos llegue una url
+    component: InicioComponent //inicie este componente
+  },
+  {
+    path:"", //redireccionar la ruta principal
+    pathMatch: 'full',  //coincidencia exacta o completa
+    redirectTo: '/inicio' // nos redireccion a esta ruta especiica
+  },
+  //cargar el resto de rutas con sus modulos con lazy loading (Estrategia)
+  {
+    path: 'seguridad',
+    loadChildren: () => import("./modulos/seguridad/seguridad.module").then(x => x.SeguridadModule)
+  },
+  {
+    path: 'administracion',
+    loadChildren: () => import("./modulos/administracion/administracion.module").then(x => x.AdministracionModule)
+  },
+  {
+    path: 'seguridad',
+    loadChildren: () => import("./modulos/pedidos/pedidos.module").then(x => x.PedidosModule)
+  },
+  //comodines o rutas que no existen y puedo reenviarla a un error o ruta especifica
+  {
+    path:"**",
+    component: ErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modulos/administracion/administracion-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+
+const routes: Routes = [
+  {
+    path: 'crear-persona',
+    component: CrearPersonaComponent
+  },
+  {
+    path: 'editar-persona',
+    component: EditarPersonaComponent
+  }
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdministracionRoutingModule { }
